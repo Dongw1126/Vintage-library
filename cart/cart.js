d1ca7ea6c280af0ed038690117774ed4cd527c8c
ParangBird <seekerman@naver.com>
2021-11-05 15:32:50 +0900

    cart js modified

--- cart/cart.js
+++ cart/cart.js
@@ -1,17 +1,30 @@
 function getPrice(){
-    let price = 0;
-    let array = [];
-    priceList.forEach(v => {array.push(v.innerHTML)});
-    
-    let stringArray = array.map(v => parseInt(v.replace(",", "").replace("원", ""),10));
+    let price = 0;    
+    let stringArray = priceArray.map(v => parseInt(v.replace(",", "").replace("원", ""),10));
     stringArray.forEach(v => price += v);
     price = price.toLocaleString('ko-KR');
     console.log(price);
     totalPrice.innerHTML = "총 가격 : " + price + "원";
 }
 
-const totalPrice = document.getElementById('totalPrice');
-const priceList = document.querySelectorAll('.priceList');
+const totalPrice = document.getElementById('total-price');
+const priceList = document.querySelectorAll('.book-price');
 
-//console.log(priceList);
+let priceArray = [];
+priceList.forEach(v => {
+    priceArray.push(v.innerHTML)
+});
+console.log(priceArray);
+
+const cartTable = document.getElementById('cart-table');
+const delBtnList = document.querySelectorAll('.delete-button');
+
+delBtnList.forEach((v, idx) => v.addEventListener('click', () => {
+    console.log(idx + " 삭제 " );
+    priceArray[idx] = "0원";
+    console.log(priceArray);
+    cartTable.rows[idx+1].className = "hidden";
+    getPrice();
+}));
+
 getPrice();
